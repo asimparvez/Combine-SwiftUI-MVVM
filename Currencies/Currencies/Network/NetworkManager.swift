e1d926265ca83b2d46363e6d3b6c1c2d1d6c6693
Asim <asimparvez_91@hotmail.com>
2021-04-09 03:00:39 +0800

    Remove commented code

--- Currencies/Currencies/Network/NetworkManager.swift
+++ Currencies/Currencies/Network/NetworkManager.swift
@@ -51,46 +51,12 @@ class NetworkManager {
                 self.cache(data, endpoint: endpoint, cachePolicy: cachePolicy)
                 return response
                 
-//                if let response = try? decoder.decode(T.self, from: data) {
-//                    return response
-//                }
-//
-//                if let responseError = try? decoder.decode(ServerError.self, from: data) {
-//                    throw CurrenciesError.server(description: responseError.error.info)
-//                }else {
-//                    throw CurrenciesError.parsing(description: "Failed to parse")
-//                }
             })
             .mapError{($0 as? CurrenciesError ?? .unknown(description: "An unknown error occured"))}
             .eraseToAnyPublisher()
         
         
-        
-        //        return URLSession.shared
-        //            .dataTaskPublisher(for: URLRequest(url: endpoint))
-        //            .mapError { error in
-        //                .network(description: error.localizedDescription)
-        //            }
-        //            .flatMap(maxPublishers: .max(1)) { [unowned self] pair in
-        //                self.decode(pair.data, endpoint: endpoint, cachePolicy: cachePolicy)
-        //            }
-        //            .eraseToAnyPublisher()
     }
-    
-//    private func decode<T: Codable>(_ data: Data, endpoint: URL, cachePolicy: CachePolicy) -> AnyPublisher<T, CurrenciesError> {
-//        let decoder = JSONDecoder()
-//
-//
-//
-//        guard let object = try? decoder.decode(T.self, from: data) else {
-//            return AnyPublisher<T, CurrenciesError>.init(
-//                Result<T, CurrenciesError>.Publisher(makeError(data: data)))
-//        }
-//        cache(data, endpoint: endpoint, cachePolicy: cachePolicy)
-//        return AnyPublisher<T, CurrenciesError>(
-//            Result<T, CurrenciesError>.Publisher(object)
-//        )
-//    }
     
     private func makeError(data: Data) -> CurrenciesError {
         var error: CurrenciesError!
